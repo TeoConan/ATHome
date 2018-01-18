532dd2b9e46915bec852454ae6bfce48b1d85580
Alexandre Pouivet <alexpouivet@hotmail.fr>
2018-01-18 15:06:03 +0100

    Modification nav et img

--- res/parts/nav.php
+++ res/parts/nav.php
@@ -1,4 +1,39 @@
 <header class="block-header">
+
+<!-- Début Pop-up -->
+<div class="pop-up">
+	<div class="body_pop-up">
+		<div class="inner">
+			<img src="res/icons/logo_black.svg" class="logo" alt="logo">
+			<div class="formulaire">
+				<form action="connexion.php" method="post">
+				<div class="mail"><p>
+						<label for="email">User :</label>
+						<input type="email" name="email" class="form" id="email" placeholder="user@athome.com" max-length="40">
+					</p>
+				</div>
+				<div class="border"></div>
+				<div class="motdepasse">
+					<p>
+						<label for="password">Password :</label>
+						<input type="password" name="password" class="form" id="password" placeholder="password" maxlength="15" >
+					</p>
+				</div>
+				<div class="border"></div>
+					<?php
+					$but_connect = new Button('Connexion', '#');
+					$but_connect->setID('button_connexion');
+					echo($but_connect->getOutput());
+					?>
+				</form>	
+				<a href="#" class="inscription">Inscription</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Fin Pop-up -->
+
+
 	<div class="inner-header">
 		<div class="item-header">
 			<div class="logos">
